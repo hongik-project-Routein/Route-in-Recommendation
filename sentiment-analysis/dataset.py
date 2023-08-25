676576f5823e3bc1bda22958657a25d69ee68ffc
Soomin Julia <34619357+soomineom@users.noreply.github.com>
2023-08-25 09:04:31 +0900

    Update dataset.py

--- sentiment-analysis/dataset.py
+++ sentiment-analysis/dataset.py
@@ -1,4 +1,5 @@
 from sklearn.feature_extraction.text import TfidfTransformer
+import urllib.request
 
 df1 = pd.read_csv('/home/smin/smin-test/route-in/csv/review_data.csv')
 df2 = pd.read_csv('/home/smin/smin-test/route-in/csv/review_data2.csv')
@@ -23,6 +24,15 @@ df2.drop("origin_reivew", axis=1, inplace=True)
 df2.rename(columns = {"senti_int" : "sentiment_score"}, inplace = True)
 df2 = df2[['clean_review', 'sentiment_score']]
 
+urllib.request.urlretrieve("https://raw.githubusercontent.com/bab2min/corpus/master/sentiment/naver_shopping.txt", filename="/home/smin/smin-test/route-in/csv/shop_rating.txt")
+shop_df = pd.read_table('shop_rating.txt', names=['ratings', 'origin_reivew'])
+shop_df.drop_duplicates(subset=['origin_reivew'], inplace=True) # 중복제거
+shop_df["clean_review"] = shop_df["origin_reivew"].apply(lambda x : text_clearing(x))
+shop_df.drop("origin_reivew", axis=1, inplace=True)
+shop_df['sentiment_score'] = shop_df['ratings'].apply(lambda x: 0 if x <= 3 else 1) # 리뷰점수 3 이하는 부정(0)으로, 4이상은 긍정(1)
+shop_df = shop_df[['clean_review', 'sentiment_score']]
 
 # 데이터셋 합치기
-all_df = pd.concat([df1, df2]).reset_index(drop=True)
+pdList = [df1, df2, shop_df]
+all_df = pd.concat(pdList).reset_index(drop=True)
+
