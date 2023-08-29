ff6d12fe41214eb4e26c2a9d3bd211ec4834a7c4
Soomin Julia <34619357+soomineom@users.noreply.github.com>
2023-08-30 08:38:25 +0900

    Create save-to-bq.py

---
+++ sentiment-analysis/save-to-bq.py
@@ -0,0 +1,23 @@
+from google.cloud import bigquery
+client = bigquery.Client()
+
+table_id = "carbon-inkwell-290604.route_in.sentimental-analysis"
+
+def save_sa(row):
+    userId = row[0]
+    mapId = row[1]
+    score = row[2]
+
+    rows_to_insert = [
+    {"userId": userId, "mapId": mapId, "score": score}
+    ]
+
+    errors = client.insert_rows_json(
+        table_id, rows_to_insert, row_ids=[None] * len(rows_to_insert)
+    )  # Make an API request.
+    if errors == []:
+        print("New rows have been added.")
+    else:
+        print("Encountered errors while inserting rows: {}".format(errors))
+
+    return "Complete def save_sa"
