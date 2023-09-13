021dd2e58de3b777a5146c36c3432b88668523c3
soomin <tnals5718@gmail.com>
2023-09-14 08:07:41 +0900

    2차원리스트

--- sentiment-analysis/save-to-bq.py
+++ sentiment-analysis/save-to-bq.py
@@ -3,21 +3,28 @@ client = bigquery.Client()
 
 table_id = "carbon-inkwell-290604.route_in.sentimental-analysis"
 
-def save_sa(row):
-    userId = row[0]
-    mapId = row[1]
-    score = row[2]
+def save_sa(row_list):
+    for row in row_list:
+        userId = row[0]
+        mapId = row[1]
+        score = row[2]
 
-    rows_to_insert = [
-    {"userId": userId, "mapId": mapId, "score": score}
-    ]
+        rows_to_insert = [
+        {"userId": userId, "mapId": mapId, "score": score}
+        ]
 
-    errors = client.insert_rows_json(
-        table_id, rows_to_insert, row_ids=[None] * len(rows_to_insert)
-    )  # Make an API request.
-    if errors == []:
-        print("New rows have been added.")
-    else:
-        print("Encountered errors while inserting rows: {}".format(errors))
+        errors = client.insert_rows_json(
+            table_id, rows_to_insert, row_ids=[None] * len(rows_to_insert)
+        )  # Make an API request.
+        if errors == []:
+            print("New rows have been added.")
+        else:
+            print("Encountered errors while inserting rows: {}".format(errors))
 
     return "Complete def save_sa"
+
+## Test
+test_list = [
+    ['user3', '10361948', '0'], ['user1', '10361948', '1'], ['user5', '10361948', '1'], ['user7', '10361948', '0'], ['user2', '10361948', '1']
+    ]
+save_sa(test_list)
